4500473314bd327e40e12f4ebadd8ff43a71ba63
Aaron Motacek <78520107+aaron-mota@users.noreply.github.com>
2023-12-21 21:22:09 -0600

    created typography component (shadcn style; still needs some TypeScript work)

---
+++ src/app/typography/page.tsx
@@ -0,0 +1,9 @@
+import TypographyVariants from "@/components/ui-other/typography-variants";
+
+export default async function Page() {
+  return (
+    <div>
+      <TypographyVariants />
+    </div>
+  );
+}
---
+++ src/components/ui-other/typography-variants.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Typography from "./typography"; // Assume this is the path to your Typography component
+
+const TypographyVariants = () => {
+  return (
+    <div className="">
+      <Typography variant="h1" gutterBottom>
+        Heading 1
+      </Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3">Heading 3</Typography>
+      <Typography variant="h4">Heading 4</Typography>
+      <Typography variant="h5">Heading 5</Typography>
+      <Typography variant="h6">Heading 6</Typography>
+      <Typography variant="p">This is a paragraph.</Typography>
+      <Typography variant="lead">This is a lead paragraph.</Typography>
+      <Typography variant="large">This is large text.</Typography>
+      <Typography variant="small">This is small text.</Typography>
+      <Typography variant="muted">This is muted text.</Typography>
+      <Typography variant="blockquote">This is a blockquote.</Typography>
+      <Typography variant="code">This is inline code.</Typography>
+      <Typography variant="ul">
+        <Typography variant="li">List item 1</Typography>
+        <Typography variant="li">List item 2</Typography>
+        <Typography variant="li">List item 3</Typography>
+      </Typography>
+    </div>
+  );
+};
+
+export default TypographyVariants;
